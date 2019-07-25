7d6701a758d132aa52fde46bbd481d193da95553
unknown <kyleteeter12@gmail.com>
2019-07-24 20:55:04 -0500

    Implement djb2

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -25,7 +25,12 @@ class BasicHashTable:
 # Research and implement the djb2 hash function
 # '''
 def hash(string, max):
-    pass
+  hash = 5381
+  
+  for character in string:
+    hash = (( hash << 2) + hash) + ord(character)
+   
+  return hash % max
 
 
 # '''
